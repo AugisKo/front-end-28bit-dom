026be3b47a3f2158fa039f8a81f0d79b59e13f27
Eugenijus <augisk@gmail.com>
2021-06-03 19:03:22 +0300

    Hamburgeris

---
+++ Samples/main.js_5
@@ -0,0 +1,24 @@
+/* 
+surasti h1 (antraste)
+Perrasyk h1 elemento teksta i "Zuikio pomidoru darzas"
+Padaryk, jog h1 elementas turetu css class "pavadinimas"
+
+*/
+
+const h1DOM = document.querySelector('h1');
+h1DOM.innerText = 'Zuikio pomidoras darzas';
+h1DOM.classList.add('pavadinimas');
+
+/*
+
+Surasti visas darzoves
+Kiekvienos darzoves teksta pakeisti i Pomidoras";
+Kiekvienai darzovei suteikti CSS class "pomidoras"
+
+*/
+
+const liDOM = document.querySelectorAll('li');
+for (let i = 0; i < liDOM.length; i++) {
+  liDOM[i].innerText = 'Pomidoras';
+  liDOM[i].classList.add('pomidoras');
+}
--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
-/* 
-surasti h1 (antraste)
-Perrasyk h1 elemento teksta i "Zuikio pomidoru darzas"
-Padaryk, jog h1 elementas turetu css class "pavadinimas"
+const logoDOM = document.querySelector('.logo');
+const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector('nav');
 
-*/
+console.log(logoDOM);
+console.log(hamburgerDOM);
+console.log(navDOM);
 
-const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio pomidoras darzas';
-h1DOM.classList.add('pavadinimas');
-
-/*
-
-Surasti visas darzoves
-Kiekvienos darzoves teksta pakeisti i Pomidoras";
-Kiekvienai darzovei suteikti CSS class "pomidoras"
-
-*/
-
-const liDOM = document.querySelectorAll('li');
-for (let i = 0; i < liDOM.length; i++) {
-  liDOM[i].innerText = 'Pomidoras';
-  liDOM[i].classList.add('pomidoras');
-}
+hamburgerDOM.addEventListener('click', () => {
+  logoDOM.classList.toggle('big');
+  navDOM.classList.toggle('visible');
+});

787b67bcd659fcdbed84c949ecf919ccd2dfa282
Eugenijus <augisk@gmail.com>
2021-06-05 10:39:23 +0300

    Messages with colors

--- js/main.js
+++ js/main.js
@@ -1,36 +1,19 @@
-let rezultatas = 0;
+const buttonDOM = document.querySelector('button');
+const inputMessageDOM = document.querySelector('#message');
+const spanDOM = document.querySelector('span');
+const inputBgColorDOM = document.querySelector('#bg-color');
+const inputTxtColorDOM = document.querySelector('#text-color');
 
-const minusDOM = document.querySelector('.minus');
-const plusDOM = document.querySelector('.plus');
-const numberDOM = document.querySelector('.number');
-const resetDOM = document.querySelector('.reset');
-const h1DOM = document.querySelector('h1');
-const ulDOM = document.querySelector('ul');
-const h1 = h1DOM.innerText;
-const h1a = 'Žaidimas progrese';
-const ulReset = ulDOM.innerHTML;
-ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`;
-const ul = ulDOM.innerHTML;
+inputMessageDOM.style.color = 'red';
 
-minusDOM.addEventListener('click', () => {
-  /* rezultatas= rezultatas - 1; arba rezultatas -= 1; arba --rezultatas */
-  numberDOM.innerText = --rezultatas;
-  h1DOM.innerText = h1a;
-  ulDOM.innerHTML = ulReset;
-  ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
-});
-
-plusDOM.addEventListener('click', () => {
-  /* rezultatas= rezultatas + 1; arba rezultatas += 1; arba ++rezultatas */
-  numberDOM.innerText = ++rezultatas;
-  h1DOM.innerText = h1a;
-  ulDOM.innerHTML = ulReset;
-  ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
-});
-
-resetDOM.addEventListener('click', () => {
-  rezultatas = 0;
-  numberDOM.innerText = rezultatas;
-  h1DOM.innerText = h1;
-  ulDOM.innerHTML = ul;
+buttonDOM.addEventListener('click', (event) => {
+  event.preventDefault();
+  bgColor = inputBgColorDOM.value;
+  (TxtColor = inputTxtColorDOM), value;
+  spanDOM.innerText = inputMessageDOM.value;
+  spanDOM.style.color = TxtColor;
+  spanDOM.style.backgroundColor = bgColor;
+  inputMessageDOM.value = '';
+  inputBgColorDOM.value = '';
+  inputTxtColorDOM.value = '';
 });

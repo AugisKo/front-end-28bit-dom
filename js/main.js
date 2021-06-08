aeec075da0b6e787972301881a6ec10b58437807
Eugenijus <augisk@gmail.com>
2021-06-08 21:12:35 +0300

    Iconos

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-function generuotiLenta(selector, count) {
-  const lentaDOM = document.querySelector(selector);
+function generuotiSocials() {}
 
-  if (!lentaDOM) {
-    console.error('ERROR: nerastas elementas');
-    return false;
-  }
-  if (count < 0 || count % 1 !== 0) {
-    console.error('ERROR: skaicius turi buti teigiamas ir sveikasis');
-    return false;
-  }
-  let HTML = '';
-  for (let i = 1; i <= count; i++) {
-    HTML += `<div>${i}</div>`;
-  }
-  lentaDOM.innerHTML = HTML;
-  console.log(lentaDOM);
-}
-
-generuotiLenta('div', 8);
+const icons = ['facebook', 'twitter', 'linkedin'];
+geberuotiSocials('.socials', icons);

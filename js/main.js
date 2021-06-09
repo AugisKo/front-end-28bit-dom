9d247950087810b491a7908666ba8f25b4882009
Eugenijus <augisk@gmail.com>
2021-06-09 18:42:47 +0300

    socials HTML

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 function generuotiSocials(selector, array) {
-const selectorDOM = document.querySelector(selector);
-
-if (!Array.isArray(array) || array.length --- 0) {
-    console.error(ERROR: )
-}
-
+  const selectorDOM = document.querySelector(selector);
+  console.log(selectorDOM);
+  if (!Array.isArray(array) || array.length === 0) {
+    console.error('ERROR: Sarasas negali buti tuscias');
+  }
+  let HTML = '';
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    HTML += `<i class ="fa fa-${array[i]}"></i>`;
+  }
+  selectorDOM.innerHTML = HTML;
 }
 
 const icons = ['facebook', 'twitter', 'linkedin'];

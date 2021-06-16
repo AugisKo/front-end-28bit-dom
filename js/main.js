c9a35e499a07087e1e0a1d1ce223cfc475b5d36d
Eugenijus <augisk@gmail.com>
2021-06-16 18:44:21 +0300

    Laikrodzio patobulinimas

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-function time(hours, interval) {
-  for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 4; j++) {
-      console.log(`${i}:${j * 15}`);
-    }
+function time(nuo, iki, trukme) {
+  let pakeistasIki = iki;
+  if (nuo > iki) {
+    pakeistasIki = 24 + iki;
+  }
+  for (let i = nuo * 60; i <= pakeistasIki * 60; i += trukme) {
+    const h = Math.floor(i / 60);
+    const m = i - h * 60;
+    const mH = h % 24;
+    console.log(`${mH < 10 ? '0' + mH : mH}:${m < 10 ? '0' + m : m}`);
   }
 }
-time(24, 15);
+
+time(22, 3, 45);
 
 /*
-j = 0  > 0 min
-j = 1 > 15
-j = 2 > 30
-j = 3 > 45
-*/
+
+8:00
+8:45
+9:30
+10:15
+11:00
+11:45
+12:30
+
+
+    */
